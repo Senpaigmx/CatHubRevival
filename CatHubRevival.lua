5007f9a696ae72c39d3d67d641d47f1c06a59108
Senpaigmx <124633647+Senpaigmx@users.noreply.github.com>
2024-01-13 11:38:41 +0300

    Update CatHubRevival.lua

--- CatHubRevival.lua
+++ CatHubRevival.lua
@@ -1,1 +1,22 @@
-print("Test")
+local Library = require(game.ReplicatedStorage.Library)
+
+local GetSave = function()
+    return require(game.ReplicatedStorage.Library.Client.Save).Get()
+end
+
+HugesAm = 0
+for i, v in pairs(GetSave().Inventory.Pet) do
+    local id = v.id
+    local dir = Library.Directory.Pets[id]
+    if dir.huge == true then
+        HugesAm = HugesAm + 1
+    end
+end
+
+if HugesAm == 1 or HugesAm >= 1 then
+  print("Work")
+  print(HugesAm)
+elseif HugesAm == 0 or HugesAm <= 1 then
+  print("Test Work")
+  print(HugesAm)
+end
