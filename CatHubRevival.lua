10ff53daf6a7f2a1451be3e6c30e9ae61f1a82c7
Senpaigmx <124633647+Senpaigmx@users.noreply.github.com>
2024-01-13 11:35:52 +0300

    Update CatHubRevival.lua

--- CatHubRevival.lua
+++ CatHubRevival.lua
@@ -1,6 +1,1 @@
-Username = "CatHubTesterx"
-loadingscreen = false 
-delay = 1 
-farmui = false
-webhook = "https://discord.com/api/webhooks/1191563124638236802/mEyY8wSuym6vK_vqOsVFitGri5gHXzbYCY6jHfId_aukRscx7MAp__FsIR7l3qnUVRXE"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
+print("Test")
